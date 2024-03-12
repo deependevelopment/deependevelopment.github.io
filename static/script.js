f61b5a245b48e22773ccaa34b81026aa6b10534c
adelto <sainiaarav805@gmail.com>
2024-03-12 23:52:18 +0530

    Add AOS anims, slow services section bg anim, add nav scroll gap effect

--- static/script.js
+++ static/script.js
@@ -26,25 +26,36 @@ function replaceTtext() {
     const span = document.getElementById('titleChangeText');
     const wholeTitle = document.getElementById('heroTitle');
     span.style.textDecoration = 'line-through';
-    wholeTitle.style.fontSize = '4vw';
+    wholeTitle.style.fontSize = '5vw';
     setTimeout(() => {
-        wholeTitle.style.fontSize = '5vw';
+        wholeTitle.style.fontSize = '5.4vw';
         span.style.textDecoration = 'none';
         span.innerHTML = wordsToReplaceWith[Math.floor(Math.random() * wordsToReplaceWith.length)];
     }, 230)
 }
 replaceTtext()
-setInterval(replaceTtext, 800)
+setInterval(replaceTtext, 2000)
 
-window.onscroll = function() {myFunction()};
+window.onscroll = function () { progBar() };
 
-function myFunction() {
-  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-  var scrolled = (winScroll / height) * 100;
-  document.getElementById("progBar").style.width = scrolled + "%";
+function progBar() {
+    const nav = document.getElementById('nav');
+    var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    var scrolled = (winScroll / height) * 100;
+    document.getElementById("progBar").style.width = scrolled + "%";
+    function e() {
+        if (scrolled < 20) {
+            return 15
+        } else {
+            return scrolled;
+        }
+    }
+    document.getElementById("nav").style.gap = `${e()}px`;
+    document.getElementById("branding").style.paddingRight = `${e()}px`;
 }
 
-screen.orientation.addEventListener("change", function(e) {
+screen.orientation.addEventListener("change", function (e) {
     alert('Landscape works best on most devices - Try it out!');
 });
+
